0d27bd129ba0e9b25b3891c007fd7275190e9489
ManHyin <111557515+ManHyin@users.noreply.github.com>
2022-10-15 16:57:13 +0800

    Add files via upload

--- Assignment2.js
+++ Assignment2.js
@@ -15,3 +15,9 @@ vegaEmbed("#line", vg_3).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "QuaterlyPercentageChange.json";
+vegaEmbed("#scatter", vg_3).then(function (result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
