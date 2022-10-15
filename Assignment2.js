738f823b270e530ee65f72ced83501df848f6dc9
ManHyin <111557515+ManHyin@users.noreply.github.com>
2022-10-15 18:51:25 +0800

    Add files via upload

--- Assignment2.js
+++ Assignment2.js
@@ -1,23 +1,23 @@
 var vg_1 = "Inflation2021.json";
-vegaEmbed("#map", vg_1).then(function (result) {
+vegaEmbed("#map", vg_1, { "actions": false }).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "GoodsandServices.json";
-vegaEmbed("#bar", vg_2).then(function (result) {
+vegaEmbed("#bar", vg_2, { "actions": false }).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "CPIPercentageChange.json";
-vegaEmbed("#line", vg_3).then(function (result) {
+vegaEmbed("#line", vg_3, { "actions": false }).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "QuaterlyPercentageChange.json";
-vegaEmbed("#scatter", vg_3).then(function (result) {
+vegaEmbed("#scatter", vg_3, { "actions": false }).then(function (result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
